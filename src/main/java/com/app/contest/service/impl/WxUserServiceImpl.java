0288d2586fe901c2315fd596ae30e93a443899a2
hirCodd <hefengen@hotmail.com>
2020-05-07 10:44:17 +0800

    update

--- src/main/java/com/app/contest/service/impl/WxUserServiceImpl.java
+++ src/main/java/com/app/contest/service/impl/WxUserServiceImpl.java
@@ -5,6 +5,7 @@ import com.app.contest.dao.WxUserDao;
 import com.app.contest.entity.WxUser;
 import com.app.contest.pojo.WxConfig;
 import com.app.contest.service.WxUserService;
+import com.app.contest.utils.SslUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Service
 public class WxUserServiceImpl implements WxUserService {
@@ -32,8 +36,17 @@ public class WxUserServiceImpl implements WxUserService {
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid="
                 + wxConfig.getAppId() + "&secret=" + wxConfig.getSecret()
                 + "&js_code="+code + "&grant_type="+wxConfig.getAuthCode();
-
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        logger.info("this url:" + url);
+        URL realUrl = null;
+        try {
+            realUrl = new URL(url);
+            if("https".equalsIgnoreCase(realUrl.getProtocol())){
+                SslUtils.ignoreSsl();
+            }
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+        ResponseEntity<String> response = restTemplate.getForEntity(String.valueOf(realUrl), String.class);
 
         logger.info(response.toString());
         return response.getStatusCode() == HttpStatus.OK ? response.getBody() : null;
